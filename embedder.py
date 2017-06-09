8f73dade369abebc61b8fb3dcec406bc254e3507
서일근 <seoilgun@gmail.com>
2017-06-09 11:19:06 +0900

    mnist test

--- embedder.py
+++ embedder.py
@@ -27,6 +27,12 @@ def summary_embedding(sess, dataset, labels, layer_outputs, image_size, channel,
 
     config = projector.ProjectorConfig()
     embed_tensors = []
+    #
+    embed_tensor = tf.Variable(dataset, name='dataset')
+    embed_tensors.append(embed_tensor)
+    sess.run(embed_tensor.initializer)
+    write_projector_config(config, embed_tensor.name, output_path, image_size, channel, summary_writer, labels)
+    #
     for embed_idx, embed_vectors in enumerate(layer_outputs):
         tensor_name = make_embed_tensor(sess, embed_vectors, embed_idx, embed_tensors)
         write_projector_config(config, tensor_name, output_path, image_size, channel, summary_writer, labels)

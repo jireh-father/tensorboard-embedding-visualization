77ad211fb5a4a77e5e34e39d78d83648369b8d5a
서일근 <seoilgun@gmail.com>
2017-06-09 11:43:30 +0900

    mnist test

--- test_mnist.py
+++ test_mnist.py
@@ -16,33 +16,6 @@ tf.app.flags.DEFINE_string('test_path', "/tmp/embed_test", "path")
 if not os.path.exists(FLAGS.test_path):
     os.makedirs(FLAGS.test_path)
 
-input_placeholder = tf.placeholder(tf.float32, shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
-
-conv1_weights = tf.Variable(tf.truncated_normal([5, 5, NUM_CHANNELS, 32], stddev=0.1, dtype=tf.float32))
-conv1_biases = tf.Variable(tf.zeros([32], dtype=tf.float32))
-conv2_weights = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1, dtype=tf.float32))
-conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=tf.float32))
-fc1_weights = tf.Variable(
-    tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512], stddev=0.1, dtype=tf.float32))
-fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=tf.float32))
-fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS], stddev=0.1, dtype=tf.float32))
-fc2_biases = tf.Variable(tf.constant(0.1, shape=[NUM_LABELS], dtype=tf.float32))
-
-
-def model(data):
-    conv = tf.nn.conv2d(data, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
-    relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
-    pool = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    conv = tf.nn.conv2d(pool, conv2_weights, strides=[1, 1, 1, 1], padding='SAME')
-    relu = tf.nn.relu(tf.nn.bias_add(conv, conv2_biases))
-    pool = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    pool_shape = pool.get_shape().as_list()
-    reshape = tf.reshape(pool, [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
-    hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
-
-    return tf.matmul(hidden, fc2_weights) + fc2_biases
-
-
 data_sets = input_data.read_data_sets(FLAGS.test_path, validation_size=BATCH_SIZE)
 
 sess = tf.Session()
@@ -51,9 +24,7 @@ sess.run(tf.global_variables_initializer())
 saver = tf.train.Saver()
 saver.restore(sess, os.path.join(FLAGS.test_path, 'model.ckpt'))
 
-logits = model(input_placeholder)
 batch_dataset, batch_labels = data_sets.validation.next_batch(BATCH_SIZE)
 batch_dataset = batch_dataset.reshape((-1, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS)).astype(np.float32)
 
-embedder.summary_embedding_with_labels(sess, batch_dataset, batch_labels, input_placeholder, FLAGS.test_path,
-                                       IMAGE_SIZE, NUM_CHANNELS, BATCH_SIZE)
+embedder.summary_embedding_with_labels(sess, batch_dataset, batch_labels, FLAGS.test_path, IMAGE_SIZE, NUM_CHANNELS)

c77a05c1d9f8fe5d6ea85182abf4fbc50242d364
서일근 <seoilgun@gmail.com>
2017-06-09 11:08:05 +0900

    mnist test

--- test_mnist.py
+++ test_mnist.py
@@ -18,33 +18,29 @@ if not os.path.exists(FLAGS.test_path):
 
 input_placeholder = tf.placeholder(tf.float32, shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
 
-conv1_weights = tf.Variable(tf.truncated_normal([5, 5, NUM_CHANNELS, 32], stddev=0.1, dtype=tf.float32),
-                            name='conv1_weights')
-conv1_biases = tf.Variable(tf.zeros([32], dtype=tf.float32), name='conv1_biases')
-conv2_weights = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1, dtype=tf.float32),
-                            name='conv2_weights')
-conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=tf.float32), name='conv2_biases')
+conv1_weights = tf.Variable(tf.truncated_normal([5, 5, NUM_CHANNELS, 32], stddev=0.1, dtype=tf.float32))
+conv1_biases = tf.Variable(tf.zeros([32], dtype=tf.float32))
+conv2_weights = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1, dtype=tf.float32))
+conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=tf.float32))
 fc1_weights = tf.Variable(
-    tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512], stddev=0.1, dtype=tf.float32),
-    name='fc1_weights')
-fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=tf.float32), name='fc1_biases')
-fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS], stddev=0.1, dtype=tf.float32),
-                          name='fc2_weights')
-fc2_biases = tf.Variable(tf.constant(0.1, shape=[NUM_LABELS], dtype=tf.float32), name='fc2_biases')
+    tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512], stddev=0.1, dtype=tf.float32))
+fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=tf.float32))
+fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS], stddev=0.1, dtype=tf.float32))
+fc2_biases = tf.Variable(tf.constant(0.1, shape=[NUM_LABELS], dtype=tf.float32))
 
 
 def model(data):
-    conv = tf.nn.conv2d(data, conv1_weights, strides=[1, 1, 1, 1], padding='SAME', name='conv1')
-    relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases), name='relu1')
-    pool = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool1')
-    conv = tf.nn.conv2d(pool, conv2_weights, strides=[1, 1, 1, 1], padding='SAME', name='conv2')
-    relu = tf.nn.relu(tf.nn.bias_add(conv, conv2_biases), name='relu2')
-    pool = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+    conv = tf.nn.conv2d(data, conv1_weights, strides=[1, 1, 1, 1], padding='SAME')
+    relu = tf.nn.relu(tf.nn.bias_add(conv, conv1_biases))
+    pool = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    conv = tf.nn.conv2d(pool, conv2_weights, strides=[1, 1, 1, 1], padding='SAME')
+    relu = tf.nn.relu(tf.nn.bias_add(conv, conv2_biases))
+    pool = tf.nn.max_pool(relu, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
     pool_shape = pool.get_shape().as_list()
-    reshape = tf.reshape(pool, [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]], name='reshape')
-    hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases, name='relu3')
+    reshape = tf.reshape(pool, [pool_shape[0], pool_shape[1] * pool_shape[2] * pool_shape[3]])
+    hidden = tf.nn.relu(tf.matmul(reshape, fc1_weights) + fc1_biases)
 
-    return tf.matmul(hidden, fc2_weights, name='matmul') + fc2_biases, conv
+    return tf.matmul(hidden, fc2_weights) + fc2_biases, conv
 
 
 data_sets = input_data.read_data_sets(FLAGS.test_path, validation_size=BATCH_SIZE)

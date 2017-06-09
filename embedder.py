e6ff97eca60e8b3083bb74078ef37e646aebcbe7
서일근 <seoilgun@gmail.com>
2017-06-09 11:49:16 +0900

    mnist test

--- embedder.py
+++ embedder.py
@@ -5,13 +5,11 @@ import numpy as np
 import tensorflow as tf
 
 
-def summary_embedding_with_labels(sess, dataset, labels, input_placeholder, summary_dir, image_size,
-                                  channel=3, batch_size=100):
+def summary_embedding_with_labels(sess, dataset, labels, summary_dir, image_size, channel=3):
     summary_embedding(sess, dataset, image_size, channel, summary_dir, labels)
 
 
-def summary_embedding_no_labels(sess, dataset, input_placeholder, summary_dir, image_size,
-                                argmax_op=None, channel=3, batch_size=100):
+def summary_embedding_no_labels(sess, dataset, summary_dir, image_size, channel=3):
     summary_embedding(sess, dataset, image_size, channel, summary_dir)
 
 
@@ -22,21 +20,12 @@ def summary_embedding(sess, dataset, image_size, channel, output_path, labels=No
     summary_writer = tf.summary.FileWriter(output_path, sess.graph)
 
     config = projector.ProjectorConfig()
-
-    # enbed_dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
-    # embed_tensor = tf.Variable(enbed_dataset, name='dataset')
-    # embed_tensors.append(embed_tensor)
-    # sess.run(embed_tensor.initializer)
-
     tensor_name = make_embed_tensor(sess, dataset, image_size)
     write_projector_config(config, tensor_name, output_path, image_size, channel, summary_writer, labels)
-    #
-    # for embed_idx, embed_vectors in enumerate(layer_outputs):
-    #     tensor_name = make_embed_tensor(sess, embed_vectors, embed_idx, embed_tensors)
-    #     write_projector_config(config, tensor_name, output_path, image_size, channel, summary_writer, labels)
+
     summary_writer.close()
 
-    save_model(sess, None, output_path)
+    save_model(sess, output_path)
 
     # Make sprite and labels.
     make_sprite(dataset, image_size, channel, output_path)
@@ -56,10 +45,10 @@ def images_to_sprite(data):
     if len(data.shape) == 3:
         data = np.tile(data[..., np.newaxis], (1, 1, 1, 3))
     data = data.astype(np.float32)
-    min = np.min(data.reshape((data.shape[0], -1)), axis=1)
-    data = (data.transpose(1, 2, 3, 0) - min).transpose(3, 0, 1, 2)
-    max = np.max(data.reshape((data.shape[0], -1)), axis=1)
-    data = (data.transpose(1, 2, 3, 0) / max).transpose(3, 0, 1, 2)
+    min_data = np.min(data.reshape((data.shape[0], -1)), axis=1)
+    data = (data.transpose(1, 2, 3, 0) - min_data).transpose(3, 0, 1, 2)
+    max_data = np.max(data.reshape((data.shape[0], -1)), axis=1)
+    data = (data.transpose(1, 2, 3, 0) / max_data).transpose(3, 0, 1, 2)
 
     n = int(np.ceil(np.sqrt(data.shape[0])))
     padding = ((0, n ** 2 - data.shape[0]), (0, 0),
@@ -67,8 +56,7 @@ def images_to_sprite(data):
     data = np.pad(data, padding, mode='constant',
                   constant_values=0)
     # Tile the individual thumbnails into an image.
-    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3)
-                                                           + tuple(range(4, data.ndim + 1)))
+    data = data.reshape((n, n) + data.shape[1:]).transpose((0, 2, 1, 3) + tuple(range(4, data.ndim + 1)))
     data = data.reshape((n * data.shape[1], n * data.shape[3]) + data.shape[4:])
     data = (data * 255).astype(np.uint8)
     return data
@@ -112,8 +100,6 @@ def write_projector_config(config, tensor_name, output_path, image_size, channel
     projector.visualize_embeddings(summary_writer, config)
 
 
-def save_model(sess, embed_tensors, output_path):
-    # sess.run(embed_tensors)
-    # saver = tf.train.Saver(embed_tensors)
+def save_model(sess, output_path):
     saver = tf.train.Saver()
     saver.save(sess, os.path.join(output_path, 'model_embed.ckpt'))
